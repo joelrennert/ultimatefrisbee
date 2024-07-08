9c9269e1d4dad473a944e1fa7419ef239d33253f
Joel R <129562199+joelrennert@users.noreply.github.com>
2024-07-07 22:45:02 -0400

    initial setup

---
+++ src/components/SquareDiv.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="square"><a href="#">🥏</a></div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+.square {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  width: 20px;
+  height: 20px;
+  font-size: 2rem;
+  padding: 23px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import SquareDiv from '../components/SquareDiv.vue'
+</script>
+
+<template>
+  <main>
+    <SquareDiv v-for="n in 1450" v-bind:key="n" class="" />
+  </main>
+</template>
+<style scoped>
+.square {
+  width: 30px;
+  height: 30px;
+}
+
+main {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  align-content: flex-start;
+  overflow-y: hidden;
+  overflow-x: hidden;
+  background: linear-gradient(
+    90deg,
+    rgba(132, 99, 174, 0.582) 0%,
+    rgba(253, 29, 29, 0.485) 40%,
+    rgba(252, 176, 69, 0.5858937324929971) 100%
+  );
+}
+</style>
