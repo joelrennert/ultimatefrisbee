1999c6ea5205b370cbe3d01419ad553eecdb022b
Joel R <129562199+joelrennert@users.noreply.github.com>
2024-10-06 21:32:04 -0400

    updated footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,23 +2,25 @@
   <footer>
     <div class="container1">
       🫠 JOEL RENNERT 2024 》
-      <a href="#">unorb.com</a> 》<a href="https://github.com/joelrennert" target="_blank"
-        >github</a
-      >
+      <a href="https://github.com/joelrennert" target="_blank">github</a>
       》<a href="https://www.linkedin.com/in/joelrennert/" target="_blank">linkedin</a>
     </div>
-    <div class="container2">thanks for looking at my vue app</div>
+    <!-- <div class="container2">amorphous rotating white balls</div> -->
   </footer>
 </template>
 <script>
 export default {}
 </script>
 <style scoped>
-.container2 {
+/* .container2 {
   display: flex;
   flex-grow: 1;
   justify-content: right;
   padding-right: 20px;
+  font-style: italic;
+} */
+.container1 {
+  z-index: 50;
 }
 
 a {
@@ -29,6 +31,7 @@ a:hover {
   background-color: white;
   font-size: 0.8rem;
   padding: 3px;
+  cursor: pointer;
 }
 
 footer {
